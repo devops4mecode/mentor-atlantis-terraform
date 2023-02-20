b1047da5b579dc9aab49e68d004b607bd0913ec8
Najib Radzuan <najibradzuan@devops4me.com>
2023-02-20 20:30:46 +0800

    changes ec2 type feature2 #14

--- provider.tf
+++ provider.tf
@@ -1,16 +1,22 @@
+# terraform {
+#   required_providers {
+#     aws = {
+#       source = "hashicorp/aws"
+#       version = ">= 4.2.0"
+#     }
+#   }
+# }
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
-      version = ">= 4.2.0"
+      source  = "hashicorp/aws"
+      version = "3.74.0"
     }
   }
 }
 
 # Configure the AWS Provider
 provider "aws" {
-  region = var.region
-  # assume_role {
-  #   role_arn = var.aws_deployment_role
-  # }
+  profile = "default"
+  region  = var.aws_region
 }
