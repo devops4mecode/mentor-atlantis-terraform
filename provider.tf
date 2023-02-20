4bf7a4fa28afb1230d7d376dcd2a21ec58e6fd85
Najib Radzuan <najibradzuan@devops4me.com>
2023-02-20 20:58:54 +0800

    changes ec2 type feature2 #18

--- provider.tf
+++ provider.tf
@@ -1,11 +1,3 @@
-# terraform {
-#   required_providers {
-#     aws = {
-#       source = "hashicorp/aws"
-#       version = ">= 4.2.0"
-#     }
-#   }
-# }
 terraform {
   required_providers {
     aws = {
@@ -17,6 +9,5 @@ terraform {
 
 # Configure the AWS Provider
 provider "aws" {
-  region  = var.region 
-  shared_credentials_files=["~/.aws/credentials"]
+  region  = var.region
 }
